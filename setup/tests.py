e5ed0c4f16cbfbf1f1fc8a4c9e951871f2c122b1
Jumbeba <jumbeba3@gmail.com>
2021-12-23 11:46:36 -0300

    Criação do app Animais, testando urls, criação da história do usuário

--- setup/tests.py
+++ setup/tests.py
@@ -9,9 +9,28 @@ class AnimaisTestCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_abre_janela_do_chrome(self):
-        self.browser.get(self.live_server_url)
+    def test_buscando_um_novo_animal(self):
+        '''
+        Teste se um usuário encontra um animal na pesquisa
+        '''
+        # Vini deseja encontrar um novo animal para adotar.
 
-    def test_deu_ruim(self):
-        '''Teste de exemplo de erro'''
-        self.fail('Teste feito para falhar. Deu ruim mesmo.')
+
+        # Ele encontra o “Busca Animal” e decide usar o site,
+        home_page = self.browser.get(self.live_server_url + '/')
+
+        # porque vê no menu do site escrito “Busca Animal”.
+        #sugestão do pycharm: brand_element = self.browser.find_elements_by('.navbar')
+        brand_element = self.browser.find_elements_by_css_selector('.navbar')
+        self.assertEqual('Busca Animal', brand_element.text)
+
+        # Ele vê um campo para pesquisar animais pelo nome, então vai digitar ali, em algum lugar vai ter um campo onde ele vai digitar o nome do animal que vai exibir
+
+
+        # Ele digita, ele pesquisar, leão, e clica no botão pesquisar.
+
+
+        # Ele desiste de adotar um leão
+
+        pass
+
